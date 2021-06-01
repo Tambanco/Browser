5592355229373af2a119599b0510b63d4cdce7f2
Al <alexunder_5542@mail.ru>
2021-06-01 12:26:56 +0300

    added WebViewController

---
+++ Browser/WebViewController.swift
@@ -0,0 +1,27 @@
+//
+//  WebViewController.swift
+//  Browser
+//
+//  Created by tambanco 🥳 on 01.06.2021.
+//
+
+import UIKit
+import WebKit
+class WebViewController: UIViewController, WKUIDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        view = webView
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let myURL = URL(string:"https://www.apple.com")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
+    }
+}
